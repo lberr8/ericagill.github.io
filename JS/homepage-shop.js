4c51ce82f9c6bce0712b47a6b9a063d741a39113
ericagill <89666180+ericagill@users.noreply.github.com>
2022-07-27 18:07:41 +0100

    Update homepage-shop.js
    update the function calling

--- JS/homepage-shop.js
+++ JS/homepage-shop.js
@@ -166,3 +166,5 @@ let calculation = () => {
   let cartIcon = document.getElementById("cartAmount");
   cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
+
+calculation();
